d711ec9ff54ad3c9b597dca3200d6bbc66213ec0
Serhiy Neskhodovskiy <serhiy@sweetstackdigital.com>
2021-08-13 17:16:26 +0300

    make jobs queueable

--- src/Jobs/BaseStorageJob.php
+++ src/Jobs/BaseStorageJob.php
@@ -2,11 +2,14 @@
 
 namespace Sweetstack\AsyncTelescope\Jobs;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Support\Collection;
 use Laravel\Telescope\Contracts\EntriesRepository;
 
 abstract class BaseStorageJob
 {
+    use Queueable;
+
     protected Collection $entries;
 
     public function __construct(Collection $entries)
