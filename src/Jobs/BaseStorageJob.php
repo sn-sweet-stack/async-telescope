530a57692f3a1d9c61985ae5709dc116e240a8c6
Serhiy Neskhodovskiy <serhiy@sweetstackdigital.com>
2021-08-13 17:24:16 +0300

    Use DatabaseEntriesRepository as eventual storage

--- src/Jobs/BaseStorageJob.php
+++ src/Jobs/BaseStorageJob.php
@@ -5,6 +5,7 @@ namespace Sweetstack\AsyncTelescope\Jobs;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Collection;
 use Laravel\Telescope\Contracts\EntriesRepository;
+use Laravel\Telescope\Storage\DatabaseEntriesRepository;
 
 abstract class BaseStorageJob
 {
@@ -19,7 +20,7 @@ abstract class BaseStorageJob
 
     protected function getDatabaseStorageDriver(): EntriesRepository
     {
-        return app(EntriesRepository::class);
+        return app(DatabaseEntriesRepository::class);
     }
 
     abstract public function handle();
