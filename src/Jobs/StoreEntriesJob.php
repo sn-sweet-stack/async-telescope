c77f53cc743370b9245e370698fdb4d1299cd8c7
Serhiy Neskhodovskiy <serhiy@sweetstackdigital.com>
2021-08-17 11:57:57 +0300

    Make jobs queueable

--- src/Jobs/StoreEntriesJob.php
+++ src/Jobs/StoreEntriesJob.php
@@ -2,8 +2,15 @@
 
 namespace Sweetstack\AsyncTelescope\Jobs;
 
-class StoreEntriesJob extends BaseStorageJob
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Bus\Queueable;
+
+class StoreEntriesJob extends BaseStorageJob implements ShouldQueue
 {
+    use Dispatchable, InteractsWithQueue, Queueable;
+
     public function handle()
     {
         $this->getDatabaseStorageDriver()->store($this->entries);
--- src/Jobs/UpdateEntriesJob.php
+++ src/Jobs/UpdateEntriesJob.php
@@ -2,8 +2,15 @@
 
 namespace Sweetstack\AsyncTelescope\Jobs;
 
-class UpdateEntriesJob extends BaseStorageJob
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Bus\Queueable;
+
+class UpdateEntriesJob extends BaseStorageJob implements ShouldQueue
 {
+    use Dispatchable, InteractsWithQueue, Queueable;
+
     public function handle()
     {
         $this->getDatabaseStorageDriver()->update($this->entries);
